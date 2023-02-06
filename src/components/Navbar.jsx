f3da1a096e8d8a4205d6ca36604b8402a5072a4c
MehmetGezen <mehmetgznarch96@gmail.com>
2023-02-06 19:35:40 +0100

    sass installed navbar styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="nav">
+        <div className="logo-div">
+          <h1 className="logo-part1">Mo</h1>
+          <h1 className="logo-part2">View</h1>
+        </div>
+        <div className="button-div">
+          <button className="nav-button">Register</button>
+          <button className="nav-button">Login</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
